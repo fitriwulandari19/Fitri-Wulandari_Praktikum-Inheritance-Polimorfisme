d55fd58a3f752f9ddcc133d948846bdba4697f7e
fitriwulandari19 <54261251+fitriwulandari19@users.noreply.github.com>
2019-09-27 11:08:43 +0700

    Add files via upload

---
+++ tugas pewarisan dengan super().py
@@ -0,0 +1,27 @@
+class SuperJunior(object):
+    def __init__(self, f, a, l):
+        self.fandom = f
+        self.anggota = a
+        self.leader = l
+
+    def cetakdata(self):
+        print("fandom\t: ", self.fandom)
+        print("anggota\t: ", self.anggota)
+        print("leader\t: ", self.leader)
+
+class SuJu(SuperJunior):
+    def __init__(self, f, a, l, m):
+        super(SuJu, self).__init__(f, a, l)
+        self.maknae = m
+
+    def cetakdata(self):
+        super(SuJu, self).cetakdata()
+        print("maknae\t: ", self.maknae)
+        
+def main():
+    SuJu1 = SuJu("ELF", "13 orang", "Leeteuk", "Kyuhyun")
+    
+    SuJu1.cetakdata()
+
+if __name__ == "__main__":
+    main()
